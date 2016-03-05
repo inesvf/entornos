69f8faa2b3c1ce23895b0e66f8db9ceab2e3759b
jcmguerrero <jcmguerrero@gmail.com>
2016-03-05 18:34:20 +0100

    Update llenar_numeros.java

--- prueba_egit/src/prueba_egit/llenar_numeros.java
+++ prueba_egit/src/prueba_egit/llenar_numeros.java
@@ -6,7 +6,7 @@ public class llenar_numeros{
 		int [] tabla=new int[n];
 		tabla=llenar(n);
 		int suma=sumar(tabla);
-		System.out.println("la suma real es: " + suma);
+		System.out.println("la suma real para jcmguerrero es: " + suma);
 		
 	}
 	private static int[] llenar (int n){
